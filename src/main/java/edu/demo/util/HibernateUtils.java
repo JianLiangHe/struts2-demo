f7c8ebe96863a9cc2f4bb27a3b10636e68897b73
Administrator <Administrator@host.docker.internal>
2020-11-17 17:24:15 +0800

    集成hibernate

---
+++ src/main/java/edu/demo/util/HibernateUtils.java
@@ -0,0 +1,41 @@
+package edu.demo.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * hibernate工具类
+ * @author Administrator
+ *
+ */
+public class HibernateUtils {
+
+	private static SessionFactory sessionFactory;
+	
+	private static Configuration configuration = new Configuration();
+	
+	private static StandardServiceRegistry serviceRegistry;
+	
+	static {
+        try {
+            configuration.configure("hibernate.cfg.xml");
+            serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            System.err.println("%%%% Error Creating SessionFactory %%%%");
+            e.printStackTrace();
+        }
+    }
+	
+	/**
+     * 获取session，所有的hibernate操作均基于session进行
+     * @return
+     */
+    public static Session getSession(){
+        return sessionFactory.openSession();
+    }
+	
+}
